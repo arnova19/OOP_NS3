a3bb3a7e0d82f71d2edf44ea98e4519112db25d7
Arnova Abdullah <arnova.abdullah19@gmail.com>
2026-02-02 15:08:14 +0100

    use IChannelModel base type to run different models

--- cpp/app/main.cpp
+++ cpp/app/main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include "AwgnChannelModel.h"
 #include "ray.h"
+#include "IChannelModel.h"
 
 int main() {
     double snr = 5.0;
     int choice = 2;   // 1 = AWGN, 2 = Rayleigh
-    double ber = 0.0;
+    
+    IChannelModel*model = nullptr;
 
     if (choice == 1) {
         AwgnChannelModel model;
         std::cout << "Using AWGN model" << std::endl;
         std::cout << "BER = " << model.EstimateBer(snr) << std::endl;
+        model = new AwgnChannelModel();
     } else {
         raychannel ray;
         std::cout << "Using Rayleigh model" << std::endl;
         std::cout << "BER = " << ray.EstimateBer(snr) << std::endl;
+        model = new raychhannel();
     }
-
+    double ber = model->EstimateBer(snr)
     std::cout << "BER = " << ber << std::endl;
     return 0;
 }
--- cpp/include/ray.h
+++ cpp/include/ray.h
@@ -1,6 +1,7 @@
 #pragma once
+#include IChannelModel.h
 
-class raychannel {
+class raychannel : public IChannelModel {
 public:
-    double EstimateBer(double snrDb);
+    double EstimateBer(double snrDb) override;
 };
