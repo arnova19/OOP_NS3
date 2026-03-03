f81ddcf01d081b439bdfbb49c3debbc1e944803d
Arnova Abdullah <102283944+arnova19@users.noreply.github.com>
2026-03-03 10:56:42 +0100

    constructor overloading

--- cpp/app/main.cpp
+++ cpp/app/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class car {         // The class
+class Car {
   public:
-    int id;
-    string name;
-    int descr;
-    string brand;// Access specifier
-    void task(int descr) {   // Method/function
-      cout << "The car is " << descr/id << " % charged with an id " << id;
+    string brand;
+    string model;
+
+    Car() {
+      brand = "Unknown";
+      model = "Unknown";
+    }
+
+    Car(string b, string m) {
+      brand = b;
+      model = m;
     }
 };
 
 int main() {
-  car car1;
-  car1.id = 12;
-  car1.name = "sedan";
-  car1.brand = "audi";// Create an object of MyClass
-  car1.task(120);  // Call the method
+  Car car1;
+  Car car2("BMW", "X5");
+  Car car3("Ford", "Mustang");
+
+  cout << "Car1: " << car1.brand << " " << car1.model << "\n";
+  cout << "Car2: " << car2.brand << " " << car2.model << "\n";
+  cout << "Car3: " << car3.brand << " " << car3.model;
   return 0;
 }
